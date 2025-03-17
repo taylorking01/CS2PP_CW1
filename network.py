9b769b0b7e8cfa0b2a3e0a7337f62ae6186e1c87
taylorking01 <taylorkinginbox@gmail.com>
2025-03-17 13:12:26 +0000

    T1 Test set 1.2 and 1.3 working - small and larger conversions

--- network.py
+++ network.py
@@ -1,6 +1,14 @@
 def file_to_edge_list(fName):
-    """
-    Minimal stub to pass test_empty_input.
-    We won't do any actual file reading yet.
-    """
-    return []
+    edge_list = []
+    with open('./data/dolphins.tsv', 'r') as file:
+        for line in file:
+            line = line.strip()
+            
+            if not line: #If the line is blank then skip this iteration.
+                continue
+
+            a_str, b_str = line.split('\t')
+            a, b = int(a_str), int(b_str)
+            edge_list.append((a,b))
+
+    return edge_list
