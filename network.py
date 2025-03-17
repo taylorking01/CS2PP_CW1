d34cb35d2997e0ad8f0c4c6e954932be8a73b3e0
taylorking01 <taylorkinginbox@gmail.com>
2025-03-17 13:42:29 +0000

    T1 Test set 2 functional - edge list to neighbour list works and tests pass

--- network.py
+++ network.py
@@ -1,6 +1,6 @@
 def file_to_edge_list(fName):
     edge_list = []
-    with open('./data/dolphins.tsv', 'r') as file:
+    with open(fName, 'r') as file:
         for line in file:
             line = line.strip()
             
@@ -12,3 +12,45 @@ def file_to_edge_list(fName):
             edge_list.append((a,b))
 
     return edge_list
+
+def edge_to_neighbour_list_1(edge_list):
+    """
+    Build a neighbour list (type is dictionary) from a list of edges (type is tuple). Implementation #1 is developed to be more efficient than #2.
+    """
+    neighbour_dict = {}
+
+    for (a, b) in edge_list:
+        if a not in neighbour_dict:
+            neighbour_dict[a] = set()
+        if b not in neighbour_dict:
+            neighbour_dict[b] = set()
+
+        neighbour_dict[a].add(b)
+        neighbour_dict[b].add(a)
+
+    return neighbour_dict
+
+def edge_to_neighbour_list_2(edge_list):
+    """
+    A less efficient approach to constructing a neighbour list using a multi-pass approach.
+    """
+    neighbour_dict = {}
+
+    #Collection of unique nodes
+    unique_nodes = set()
+    for (a, b) in edge_list:
+        unique_nodes.add(a)
+        unique_nodes.add(b)
+
+    #For each node, then find which edges are connecting to it.
+    for node in unique_nodes:
+        neighbours = set()
+        for (a, b) in edge_list:
+            if a == node:
+                neighbours.add(b)
+            elif b == node:
+                neighbours.add(a)
+        neighbour_dict[node] = neighbours
+
+    return neighbour_dict
+    
--- test_network.py
+++ test_network.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch, mock_open
 
-from network import file_to_edge_list
+from network import file_to_edge_list, edge_to_neighbour_list_1, edge_to_neighbour_list_2
 
 class TestFileToEdgeList(unittest.TestCase):
     """
@@ -37,6 +37,59 @@ class TestFileToEdgeList(unittest.TestCase):
         #Check to ensure correct number of edges and one of the known pairs exists.
         self.assertEqual(len(edges), 5, "Should have 5 edges total")
         self.assertIn((999, 1000), edges, "Edge (999, 1000) should be in the list")
+
+class TestEdgeToNeighbourList(unittest.TestCase):
+    """
+    Tests for edge_to_neighbour_list_1 and edge_to_neighbour_list_2.
+    """
+
+    def test_neighbour_list_equivalence(self):
+        """
+        Test both functions produce the same neighbor list on a known sample input.
+        """
+        edge_list = [(0, 1), (1, 2), (2, 3), (0, 3)]
+        # We'll manually define the expected neighbor list (undirected)
+        # Node 0 => {1,3}, Node 1 => {0,2}, Node 2 => {1,3}, Node 3 => {0,2}
+        expected = {
+            0: {1, 3},
+            1: {0, 2},
+            2: {1, 3},
+            3: {0, 2}
+        }
+
+        result_1 = edge_to_neighbour_list_1(edge_list)
+        result_2 = edge_to_neighbour_list_2(edge_list)
+
+        self.assertEqual(result_1, expected, "edge_to_neighbour_list_1 result is incorrect")
+        self.assertEqual(result_2, expected, "edge_to_neighbour_list_2 result is incorrect")
+
+    def test_neighbour_list_empty_input(self):
+        """
+        Test that an empty edge list returns an empty neighbor list.
+        """
+        edge_list = []
+        expected = {}
+        result_1 = edge_to_neighbour_list_1(edge_list)
+        result_2 = edge_to_neighbour_list_2(edge_list)
+        self.assertEqual(result_1, expected, "Empty edge list should give empty dict (method 1)")
+        self.assertEqual(result_2, expected, "Empty edge list should give empty dict (method 2)")
+
+    def test_neighbour_list_disconnected_nodes(self):
+        """
+        Ensure that if we have, say, edges among (0,1) and (2,3), no spurious connections appear.
+        """
+        edge_list = [(0, 1), (2, 3)]
+        # Expected neighbor list has 2 disconnected components
+        expected = {
+            0: {1},
+            1: {0},
+            2: {3},
+            3: {2}
+        }
+        result_1 = edge_to_neighbour_list_1(edge_list)
+        result_2 = edge_to_neighbour_list_2(edge_list)
+        self.assertEqual(result_1, expected, "Disconnected nodes not handled correctly (method 1)")
+        self.assertEqual(result_2, expected, "Disconnected nodes not handled correctly (method 2)")
 
 if __name__ == "__main__":
     unittest.main()
