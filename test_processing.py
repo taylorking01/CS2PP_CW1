d8f2ba3bb8e53237926f1f549471d2cf33ee5793
Taylor King <ft013085@student.reading.ac.uk>
2025-03-20 12:20:26 +0000

    T2 - process_csv commented out and ready for incremental devving.

--- test_processing.py
+++ test_processing.py
@@ -3,11 +3,15 @@
 
 import unittest
 from processing import (
-    read_csv, remove_columns, remove_makes, remove_duplicates,
-    rename_columns, replace_missing_hp_with_median, remove_rows_with_missing_values,
-    add_hp_type_column, add_price_class_column, round_price,
-    filter_year, filter_make_counts, compute_summary, write_csv, process_csv
+    read_csv
 )
+
+# from processing import (
+#     read_csv, remove_columns, remove_makes, remove_duplicates,
+#     rename_columns, replace_missing_hp_with_median, remove_rows_with_missing_values,
+#     add_hp_type_column, add_price_class_column, round_price,
+#     filter_year, filter_make_counts, compute_summary, write_csv, process_csv
+# )
 
 class TestProcessing(unittest.TestCase):
 
